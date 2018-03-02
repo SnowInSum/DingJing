8dbb785b8509003f929e6a7e7dc3b924816435a4
zjx <1363343773@qq.com>
2018-03-02 10:58:48 +0800

    20180302

---
+++ DingJing/src/EnterpriseSign/MainWidget.h
@@ -0,0 +1,39 @@
+﻿#ifndef MAINWIDGET_H
+#define MAINWIDGET_H
+
+#include <QWidget>
+#include "BusinessCard.h"
+#include "SignCard.h"
+
+namespace Ui {
+class MainWidget;
+}
+
+class MainWidget : public QWidget
+{
+    Q_OBJECT
+
+public:
+    explicit MainWidget(QWidget *parent = 0);
+    ~MainWidget();
+
+protected:
+    void InitWidgetLayout();
+
+private slots:
+    void on_admin_login_clicked();
+
+private:
+    Ui::MainWidget *ui;
+
+    int widthSize;
+    int heightSize;
+
+    BusinessCard *one;
+    BusinessCard *two;
+    BusinessCard *three;
+
+    SignCard *signPerson;
+};
+
+#endif // MAINWIDGET_H
